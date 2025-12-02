ae53437fbc83b6aa7d3b4bd2f87a648b415a6c86
Nefr0l <filip.wrzosek07@gmail.com>
2025-12-02 20:14:07 +0100

    bumping around

--- 2025/day01.go
+++ 2025/day01.go
@@ -5,61 +5,53 @@ import (
 	"fmt"
 )
 
-var pass = 50
+var curr = 50
 var zeros = 0
 
 func Day01(dict []dict.Dict) error {
 	fmt.Println(dict)
-	yes := false
 
 	for _, entry := range dict {
+
 		var zerosBefore = zeros
 
-		fmt.Printf("[ENTRY]: Value: %03d rotated by %v%02v. ", pass, entry.Key, entry.Value)
+		fmt.Printf("[ENTRY]: Value: %03d rotated by %v%02v. ", curr, entry.Key, entry.Value)
 
 		zeros += entry.Value / 100
 		entry.Value = entry.Value % 100
 
+		if entry.Value == 0 {
+			err := fmt.Errorf("lol")
+			fmt.Println(err.Error())
+			return err
+		}
+
 		switch entry.Key {
 		case "L":
-			if pass >= entry.Value {
-				fmt.Print("	[TU]	")
-				pass -= entry.Value
-
-				if pass == 0 {
-
-					// fix here
-					if yes {
-						zeros++
-						yes = false
-					} else {
-						yes = true
-					}
-
-				}
-			} else {
-				var temp = pass
-				pass = (100 - entry.Value + temp) % 100
+			if curr > entry.Value {
+				curr -= entry.Value
+			} else if curr == entry.Value {
+				curr = 0
+				zeros++
+			} else if curr < entry.Value {
+				var temp = curr
+				curr = 100 - entry.Value + temp
 				zeros++
 			}
 
 		case "R":
-			pass += entry.Value
-			if pass >= 100 {
-				pass -= 100
+			curr += entry.Value
+			if curr >= 100 {
+				curr -= 100
 				zeros++
 			}
 		}
 
-		fmt.Printf("	[END]: Zeros %02d + %02v = %02v, Rotation: %02v, Value: %00d \n", zerosBefore, zeros-zerosBefore, zeros, entry, pass)
+		fmt.Printf("	[END]: Zeros %02d + %02v = %02v, Rotation: %02v, Value: %00d \n", zerosBefore, zeros-zerosBefore, zeros, entry, curr)
 
 	}
 
 	fmt.Println(zeros)
 
 	return nil
-}
-
-func TakeTurn(rotation string) {
-
 }
