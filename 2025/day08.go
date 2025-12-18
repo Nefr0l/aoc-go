a532aba7e9a7bdc93d48597c235fc367f3e7db6a
Nefr0l <filip.wrzosek07@gmail.com>
2025-12-18 21:35:08 +0100

    bumping around

--- 2025/day08.go
+++ 2025/day08.go
@@ -4,6 +4,7 @@ import (
 	"aoc/types"
 	"fmt"
 	"math"
+	"slices"
 )
 
 type point struct {
@@ -15,62 +16,115 @@ type link struct {
 	points []point
 }
 
+type distance struct {
+	aIdx   int
+	bIdx   int
+	length float64
+}
+
 func Day08_part1(values []types.Vector3) {
 	var points []point
 	var links []*link
+	var distances []distance
 
 	for _, v := range values {
 		points = append(points, point{position: v})
 	}
 
+	// calculate closest diistances
 	for i, a := range points {
-		// calculate closest point
 		minimalDistance := 0.0
 		closestIndex := 0
 
 		for j, b := range points {
-			d := math.Sqrt(math.Pow(float64(b.position.X-a.position.X), 2) + math.Pow(float64(b.position.Y-a.position.Y), 2) + math.Pow(float64(b.position.Z-a.position.Z), 2))
-
 			if i == j {
 				continue
 			}
 
-			if minimalDistance == 0 {
-				minimalDistance = d
-				closestIndex = j
-			} else if d < minimalDistance {
+			d := math.Sqrt(math.Pow(float64(b.position.X-a.position.X), 2) + math.Pow(float64(b.position.Y-a.position.Y), 2) + math.Pow(float64(b.position.Z-a.position.Z), 2))
+
+			if minimalDistance == 0 || d < minimalDistance {
 				minimalDistance = d
 				closestIndex = j
 			}
 		}
 
-		// TODO: 10 or 1000 pairs max
-		// do something with the closest point
+		d := distance{aIdx: i, bIdx: closestIndex, length: minimalDistance}
 
-		if points[i] == points[closestIndex] {
+		idx := CanAdd(distances, d)
+		if idx == -2 {
+			distances = append(distances, d)
+		} else if idx == -1 {
+			continue
+		} else {
+			distances[idx] = d
+		}
+	}
+
+	// print distances
+	for _, d := range distances {
+		fmt.Printf("point a: %v		point b: %v		distance: %v \n", points[d.aIdx], points[d.bIdx], d.length)
+	}
+
+	// work with top 10 minimal distances
+	for _, d := range distances {
+		a := &points[d.aIdx]
+		b := &points[d.bIdx]
+
+		if a == b {
 			continue
 		}
 
-		if points[closestIndex].link == nil { // if b does not have link
+		if b.link == nil { // if b does not have link
 			var l link
-			l.points = append(l.points, a, points[closestIndex]) // maybe check for link's length ?
+			l.points = append(l.points, *a, *b)
 			links = append(links, &l)
 
-			points[i].link = &l
-			points[closestIndex].link = points[i].link
-		} else if points[closestIndex].link != points[i].link { // if b does have link and it's not the same link as a's link
-			points[closestIndex].link.points = append(points[closestIndex].link.points, points[i])
-			points[i].link = points[closestIndex].link
+			a.link = &l
+			b.link = a.link
+		} else if a.link != b.link { // if b does have link and it's not the same link as a's link
+			b.link.points = append(b.link.points, *a)
+			a.link = b.link
 		}
-
-		// print links
 
 	}
 
+	// debug prints
 	fmt.Println("")
+	fmt.Println(len(distances))
 
 	for _, l := range links {
 		fmt.Println(l)
 	}
 
+}
+
+func CanAdd(distances []distance, d distance) int {
+
+	// check length
+	if len(distances) < 10 {
+		return -2
+	}
+
+	// check duplicates
+	for _, d2 := range distances {
+		if d.aIdx == d2.bIdx && d.bIdx == d2.aIdx {
+			return -1
+		}
+	}
+
+	// check if length < max
+	var lengths []float64
+	for _, d := range distances {
+		lengths = append(lengths, d.length)
+	}
+
+	mx := slices.Max(lengths)
+	idx := slices.Index(lengths, mx)
+
+	if d.length < mx {
+		return idx
+	}
+
+	return -1
 }
