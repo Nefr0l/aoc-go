b7d94596b9173f390c4a8e2a9ea2682526e35900
Nefr0l <filip.wrzosek07@gmail.com>
2025-12-20 17:05:44 +0100

    fix: code cleanup

--- 2025/day09.go
+++ 2025/day09.go
@@ -11,11 +11,11 @@ func Day09_part1(points []types.Vector2) {
 
 	for i, p1 := range points {
 		for _, p2 := range points[i+1:] {
-			width := int(math.Abs(float64(p2.X-p1.X)) + 1)
-			height := int(math.Abs(float64(p2.Y-p1.Y)) + 1)
+			width := math.Abs(float64(p2.X-p1.X)) + 1
+			height := math.Abs(float64(p2.Y-p1.Y)) + 1
 			area := width * height
-			maxArea = max(maxArea, area)
-			//fmt.Printf("p1 is %v	p2 is %v 	area is %v\n", p1, p2, area)
+
+			maxArea = max(maxArea, int(area))
 		}
 	}
 
